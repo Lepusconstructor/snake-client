86ee449e0856b141f27cf042d2f434525d4522af
Sunny (Hui-Shan) Cheng <huishan.cheng@studio.unibo.it>
2020-11-03 20:17:25 +0000

    adding a badass message mapping to p

--- input.js
+++ input.js
@@ -13,14 +13,16 @@ const handleUserInput = (key) => {
     if (key === '\u0003') {
       process.exit();
     } else if (key === 'w') {
-        connection.write("Move: up");
+      connection.write("Move: up");
     } else if (key === 'a') {
-        connection.write("Move: left");
+      connection.write("Move: left");
     } else if (key === 's') {
-        connection.write("Move: down");
+      connection.write("Move: down");
     } else if (key === 'd') {
-        connection.write("Move: right");
-      }
+      connection.write("Move: right");
+    } else if (key === 'p') {
+      connection.write("Say: GETTOUTTA MY WAY");
+    }
   
   };
 
