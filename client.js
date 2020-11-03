2f0810c6a02b1056948d25ebe27af2943ee859ba
Sunny (Hui-Shan) Cheng <huishan.cheng@studio.unibo.it>
2020-11-03 18:04:30 +0000

    experimented with moving up,down,left,right with conn.write('Move: up') as well as setTimeout

--- client.js
+++ client.js
@@ -11,6 +11,9 @@ const connect = function() {
   conn.on("connect", () => {
     console.log("Connection to game server established!");
     conn.write("Name: BUN");
+    //conn.write("Move: up") will move the snake up automatically everytime we connect so we're not going to write it this way
+    
+
   });
 
   conn.on("data", (data) => {
